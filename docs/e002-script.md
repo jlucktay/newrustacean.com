fdc9bdcf54589028cd07704ae16fdeedde85c709
James Lucktaylor <jlucktay+github@gmail.com>
2022-08-30 17:28:06 +0100

    style(e002): wrap at 120

--- docs/e002-script.md
+++ docs/e002-script.md
@@ -2,9 +2,8 @@
 
 ## Intro (30s)
 
-Hello, I'm Chris Krycho, and this is the New Rustacean podcast---a 15--20 minute
-show about learning the Rust programming language. This is episode 2: "Something
-borrowed, something... moved?"
+Hello, I'm Chris Krycho, and this is the New Rustacean podcast---a 15--20 minute show about learning the Rust
+programming language. This is episode 2: "Something borrowed, something... moved?"
 
 ## News (4m)
 
@@ -13,18 +12,16 @@ borrowed, something... moved?"
   - ["rustfmt-ing Rust`](https://www.ncameron.org/blog/rustfmt-ing-rust/)
   - [Reddit discussion](https://www.reddit.com/r/rust/comments/3nt2vm/rustfmting_rust_please_help_me_rustfmt_the_rust/)
 
-- Open RFC for incremental compilation -- valuable to decrease iteration time,
-  especially on large projects (and available to other comparable languages).
-  Right now, Rust compiles the entirety of a given "crate" (package)
+- Open RFC for incremental compilation -- valuable to decrease iteration time, especially on large projects (and
+  available to other comparable languages). Right now, Rust compiles the entirety of a given "crate" (package)
 
   - [RFC issue](https://github.com/rust-lang/rfcs/pull/1298)
   - [RFC text](https://github.com/nikomatsakis/rfcs/blob/incremental-compilation/text/0000-incremental-compilation.md)
 
 ### About RFCs
 
-Rust, like several other communities (EmberJS), discusses suggested changes to
-the  language and tooling via "RFCs", i.e. "requests for comment". People submit
-a PR with a detailed proposal, the tradeoffs, etc. and then the community
+Rust, like several other communities (EmberJS), discusses suggested changes to the language and tooling via "RFCs",
+i.e. "requests for comment". People submit a PR with a detailed proposal, the tradeoffs, etc. and then the community
 discusses it. Once accepted, implementation.
 
 ## `struct` types (5m)
@@ -33,16 +30,15 @@ discusses it. Once accepted, implementation.
 
 - Data structures are fundamental
 - The kinds of structures you and have and what they can do empower/limit you.
-  - True of language constructs in general: can you have standalone
-    functions, or do they have to belong to a class (_a la_ Java, C♯, etc.)
-  - Can you associate behavior with them (classes in classical languages,
-    objects in e.g. JS)?
+  - True of language constructs in general: can you have standalone functions, or do they have to belong to a class (_a
+    la_ Java, C♯, etc.)
+  - Can you associate behavior with them (classes in classical languages, objects in e.g. JS)?
   - How do they relate to each other?
     - Classical inheritance
     - Prototypal inheritance
     - Mixins/traits
-- Two basic type constructors in Rust: `struct` and `enum`; today, focus on
-  `structs` (the simpler of the two). Also tuples.
+- Two basic type constructors in Rust: `struct` and `enum`; today, focus on `structs` (the simpler of the two). Also
+  tuples.
 - No inheritance (yet)
 
 ### Use
@@ -51,8 +47,7 @@ discusses it. Once accepted, implementation.
 - Members can be any type: primitives, other `struct`s, functions
 - Roughly like C++ or Java classes in many ways, but without inheritance, and
   - Rust has no header/source distinction as in C++
-    - use documentation tools, etc. instead of reaidng header files. Cf.
-      Python's `help` etc.
+    - use documentation tools, etc. instead of reaidng header files. Cf. Python's `help` etc.
   - but implementation still separate from definition (in `impl` blocks)
 - Double colon notation for methods
 - Dot notation for member access
@@ -61,38 +56,30 @@ discusses it. Once accepted, implementation.
 
 ### Concepts
 
-- A core problem in languages with memory management is knowing the state of
-  a given piece of data
+- A core problem in languages with memory management is knowing the state of a given piece of data
 - "Shared mutable state is the root of all evil"
 - Rust addresses via "ownership"
   - unlimited immutable references
   - max one mutable reference
-- When handing data to one function from another, the data can be _borrowed_
-  or _moved_.
+- When handing data to one function from another, the data can be _borrowed_ or _moved_.
 - If you _borrow_ data, you get a "reference".
-  - Not the same as other languages---though some similarities under the
-    covers
-  - Means you don't _own_ it, you just have access to it at some level. When
-    done, control goes back to caller (owner)
+  - Not the same as other languages---though some similarities under the covers
+  - Means you don't _own_ it, you just have access to it at some level. When done, control goes back to caller (owner)
 - If you _move_ data, you get _ownership_.
-  - You can do anything allowed by mutability rules of the type in the new
-    owner.
-  - On exiting the new owner's scope, the object is _dropped_---memory is
-    de-allocated.
+  - You can do anything allowed by mutability rules of the type in the new owner.
+  - On exiting the new owner's scope, the object is _dropped_---memory is de-allocated.
   - As a result, original owner can't access it anymore.
 
 ### Use
 
 - References use `&`.
-- The `*` operator dereferences, allowing you to assign new values to the
-  referenced element (e.g. a new `struct` instance entirely, not just
-  operating on the internals of a given `struct`).
+- The `*` operator dereferences, allowing you to assign new values to the referenced element (e.g. a new `struct`
+  instance entirely, not just operating on the internals of a given `struct`).
   - But as always, only for mutable references.
-  - It does a bit more than that, too; we'll come back to that in the future
-    when we talk about traits.
+  - It does a bit more than that, too; we'll come back to that in the future when we talk about traits.
 - Moves happen in function calls when using non-reference types
-- Can use `move` keyword to specify moves in other contexts as well -- more on
-  that in a future episode (when I understand it better myself!)
+- Can use `move` keyword to specify moves in other contexts as well -- more on that in a future episode (when I
+  understand it better myself!)
 
 It's both _like_ and _unlike_ C/C++/etc.
 
@@ -101,15 +88,13 @@ It's both _like_ and _unlike_ C/C++/etc.
 ### Next time
 
 - Another data structure type: `Enum`s!
-- The associated notion of pattern matching---one of the most powerful
-  concepts in Rust.
+- The associated notion of pattern matching---one of the most powerful concepts in Rust.
 - Meaningful, safe return values from functions which might fail
 
 ### Follow/support
 
 - Follow: Tw, ADN (NR, then ck)
 - Support at Patreon
-- Show notes at NewRustacean.com, with links to content, Rust reference,
-  social media, etc.
+- Show notes at NewRustacean.com, with links to content, Rust reference, social media, etc.
 
 Until next time, happy coding!
--- src/e002.rs
+++ src/e002.rs
@@ -7,22 +7,20 @@
 //!
 //! [mp3]: https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e002.mp3
 //!
-//! <audio style="width: 100%" title="Something borrowed, something... moved?" controls preload=metadata src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e002.mp3"></audio>
+//! <audio style="width: 100%" title="Something borrowed, something... moved?" controls preload=metadata
+//! src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e002.mp3"></audio>
 //!
 //! # Notes
 //!
-//! Today's episode discusses, and the associated source code demonstrates,
-//! a few basic behaviors of structs... including borrowing!
+//! Today's episode discusses, and the associated source code demonstrates, a few basic behaviors of structs...
+//! including borrowing!
 //!
-//! After taking a short look at one of Rust's basic approaches to creating new
-//! types, we dive into a *fairly* thorough overview of how borrowing works in
-//! fairly run-of-the-mill Rust code. This is a basic introduction, and as such
-//! I'm not getting into things like heap-allocated memory (`Box`) or dealing
-//! with `move` semantics with threads or closures. (I haven't actually figured
-//! those out well enough yet to write something like this for them!)
+//! After taking a short look at one of Rust's basic approaches to creating new types, we dive into a *fairly* thorough
+//! overview of how borrowing works in fairly run-of-the-mill Rust code. This is a basic introduction, and as such I'm
+//! not getting into things like heap-allocated memory (`Box`) or dealing with `move` semantics with threads or
+//! closures. (I haven't actually figured those out well enough yet to write something like this for them!)
 //!
-//! As usual, you'll want to have the [`src`] open to see what I'm doing with
-//! the components documented below.
+//! As usual, you'll want to have the [`src`] open to see what I'm doing with the components documented below.
 //!
 //! [`src`]: ../../src/show_notes/e002.rs.html
 //!
@@ -84,11 +82,9 @@ pub fn demonstrate_ownership() {
         mutable.r
     );
 
-    // If I move an object, I can no longer access it afterward. If you
-    // uncomment the following line, you'll see that the Circle instances are
-    // no longer accessible. Note that moving and borrowing are orthogonal to
-    // mutability. It's not accessible after this because it actually gets
-    // dropped, i.e. de-allocated.
+    // If I move an object, I can no longer access it afterward. If you uncomment the following line, you'll see that
+    // the Circle instances are no longer accessible. Note that moving and borrowing are orthogonal to mutability. It's
+    // not accessible after this because it actually gets dropped, i.e. de-allocated.
     move_circle(immutable);
     move_circle(mutable);
     // println!("Immutable: {:?}", immutable);
@@ -103,8 +99,7 @@ pub fn borrow(ref_to_circle: &Circle) {
         ref_to_circle.x, ref_to_circle.y, ref_to_circle.r
     );
 
-    // You can uncomment this to see the compiler error if you try to *change*
-    // the contents.
+    // You can uncomment this to see the compiler error if you try to *change* the contents.
     // ref_to_circle.r *= 2.0;
 
     // Similarly, if you try to reassign the reference itself, it will fail:
@@ -130,15 +125,14 @@ pub fn move_circle(moved_circle: Circle) {
     // Note that this is an immutable type: this won't compile.
     // moved_circle.r *= 2.0;
 
-    // We own the object, but it's still immutable, so we can't reassign the
-    // name as we like.
+    // We own the object, but it's still immutable, so we can't reassign the name as we like.
     // moved_circle = Circle::new(4.5, 9.0, 18.0);
 }
 
 /// Implement some methods on the `Circle`.
 ///
-/// This lets use demonstrate both how methods work in general and specifically
-/// how they interact with the idea of ownership.
+/// This lets use demonstrate both how methods work in general and specifically how they interact with the idea of
+/// ownership.
 impl Circle {
     /// Creates a `Circle` instance centered on the "origin" (x = 0, y = 0).
     const fn origin(r: f64) -> Self {
@@ -151,8 +145,7 @@ impl Circle {
         Self { x, y, r }
     }
 
-    /// Returns the value of `Circle.x`, borrowing an immutable reference to
-    /// the circle to do it.
+    /// Returns the value of `Circle.x`, borrowing an immutable reference to the circle to do it.
     ///
     /// Because the reference is immutable, if you tried to do this---
     ///
@@ -168,22 +161,19 @@ impl Circle {
         self.x
     }
 
-    /// Returns the value of `Circle.x`, borrowing a mutable reference to the
-    /// circle and changing the value (demonstrating a situation in which you
-    /// would want to use a mutable rather than immutable reference).
+    /// Returns the value of `Circle.x`, borrowing a mutable reference to the circle and changing the value
+    /// (demonstrating a situation in which you would want to use a mutable rather than immutable reference).
     pub fn x_by_mut_ref(&mut self) -> f64 {
         println!("Taking a mutable reference.");
         self.x += 1.0;
         self.x
     }
 
-    /// Returns the value of `Circle.x`, taking ownership of the circle. As a
-    /// result of the change in ownership, the circle goes out of scope after
-    /// the method returns, so the circle instance will be inaccessible after
-    /// that.
+    /// Returns the value of `Circle.x`, taking ownership of the circle. As a result of the change in ownership, the
+    /// circle goes out of scope after the method returns, so the circle instance will be inaccessible after that.
     ///
-    /// Note that the item is taken as immutable, so attempting to change the
-    /// internals will still fail. **Ownership is orthogonal to immutability.**
+    /// Note that the item is taken as immutable, so attempting to change the internals will still fail. **Ownership is
+    /// orthogonal to immutability.**
     #[must_use]
     pub fn by_take(self) -> f64 {
         println!("Taking ownership, not just borrowing a reference. INTENSE.");
@@ -201,8 +191,7 @@ impl Circle {
 
 /// Demonstrates how the same concepts apply when dealing with methods.
 pub fn demonstrate_method_ownership() {
-    // There are several ways to construct struct types. The first is a plain
-    // struct constructor.
+    // There are several ways to construct struct types. The first is a plain struct constructor.
     let basic = Circle {
         x: 1.0,
         y: 2.0,
@@ -216,45 +205,39 @@ pub fn demonstrate_method_ownership() {
     println!("{:?}", immutable);
     println!("{:?}", mutable);
 
-    // Now we can see how different method calls work. When we borrow a
-    // reference, there is no consequence: during the life of the method, the
-    // immutable object is "borrowed" by the `x_by_ref` method. Ownership returns
-    // to the `main` function when it returns.
+    // Now we can see how different method calls work. When we borrow a reference, there is no consequence: during the
+    // life of the method, the immutable object is "borrowed" by the `x_by_ref` method. Ownership returns to the `main`
+    // function when it returns.
     let by_ref_x = immutable.x_by_ref();
     println!("By reference: {}", by_ref_x);
 
-    // The same is true of a method that borrows a mutable reference. Note,
-    // however, that since the reference is mutable, it can change the contents
-    // of the struct.
+    // The same is true of a method that borrows a mutable reference. Note, however, that since the reference is
+    // mutable, it can change the contents of the struct.
     print!("Original x_by_mut_ref x value: {}... ", mutable.x);
     let by_mut_ref_x = mutable.x_by_mut_ref();
     println!("After calling `x_by_mut_ref`: {}", by_mut_ref_x);
 
-    // Note that both of these struct instances are still available for `main`
-    // to use once they return from the method call:
+    // Note that both of these struct instances are still available for `main` to use once they return from the method
+    // call:
     println!(
         "immutable's y: {} | mutable's r: {}",
         immutable.y, mutable.r
     );
 
-    // A method that takes ownership can still return a value. However, it has
-    // a very different behavior in terms of the struct instance, in a way that
-    // seems surprising compared to languages without the concept of "borrowing"
-    // vs. "taking" (C, Java, Python, etc.).
+    // A method that takes ownership can still return a value. However, it has a very different behavior in terms of
+    // the struct instance, in a way that seems surprising compared to languages without the concept of "borrowing" vs.
+    // "taking" (C, Java, Python, etc.).
     //
-    // Run a method that takes ownership rather than just borrowing a reference.
-    // The value returned is as expected. However, the item has been *moved*,
-    // not just borrowed. That is, `main` no longer owns it---`by_take` took
-    // over the ownership. Once `by_take` ended, `immutable` actually gets
-    // deconstructed ("dropped") because it went out of scope. As a result, the
-    // following line won't compile (you can uncomment it to see for yourself):
+    // Run a method that takes ownership rather than just borrowing a reference. The value returned is as expected.
+    // However, the item has been *moved*, not just borrowed. That is, `main` no longer owns it---`by_take` took over
+    // the ownership. Once `by_take` ended, `immutable` actually gets deconstructed ("dropped") because it went out of
+    // scope. As a result, the following line won't compile (you can uncomment it to see for yourself):
     let by_take_x = immutable.by_take();
     println!("By take: {}", by_take_x);
     // println!("Does the circle still exist? {:?}", immutable);
 
-    // Likewise, if we use the mutable circle with the method which takes
-    // ownership and mutates it, we can get the value back as before, but the
-    // circle will have been dropped when the `by_take_mut` call ended.
+    // Likewise, if we use the mutable circle with the method which takes ownership and mutates it, we can get the
+    // value back as before, but the circle will have been dropped when the `by_take_mut` call ended.
     let by_mut_take_x = mutable.by_take_mut();
     println!("By mutable take: {}", by_mut_take_x);
     // println!("Does the mutable circle still exist? {:?}", mutable);

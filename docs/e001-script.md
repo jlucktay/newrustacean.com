a0eee1eb74006e44c5e0e0bbc75fa06cfb92b9ad
James Lucktaylor <jlucktay+github@gmail.com>
2022-08-30 17:17:17 +0100

    style(e001): wrap at 120

--- docs/e001-script.md
+++ docs/e001-script.md
@@ -2,30 +2,27 @@
 
 ## Intro
 
-Hello, I'm Chris Krycho and this is the New Rustacean podcast, a 15--20 minute
-show about learning the Rust Programming Language. This is episode 1.
+Hello, I'm Chris Krycho and this is the New Rustacean podcast, a 15--20 minute show about learning the Rust Programming
+Language. This is episode 1.
 
 ## Fun News
 
-MSVC integration (the first pieces of which landed in 1.3) are coming along in
-the 1.4 beta (should land at the end of October). Huge for being able to use it
-with existing C codebases on Windows (like, for example, in one of my ongoing
+MSVC integration (the first pieces of which landed in 1.3) are coming along in the 1.4 beta (should land at the end of
+October). Huge for being able to use it with existing C codebases on Windows (like, for example, in one of my ongoing
 contracts).
 
 ## Docs
 
 ### Why do we care?
 
-- Just plain handy. No need to use external tools (Sphinx, Doxygen, JSDoc,
-  etc.) because it has language-level support.
+- Just plain handy. No need to use external tools (Sphinx, Doxygen, JSDoc, etc.) because it has language-level support.
   - Predecessors: Python, Java
   - Modern, similar tooling: Elixir, Julia
 
-- Shapes the community: doesn't guarantee anything, but it makes good docs
-  *likelier*.
+- Shapes the community: doesn't guarantee anything, but it makes good docs *likelier*.
 
-- Standardizes the docs. Python docs are all over the map---wildly different
-  layouts, themes, etc. Finding things is hard
+- Standardizes the docs. Python docs are all over the map---wildly different layouts, themes, etc. Finding things is
+  hard.
 
 ### `rustdoc`
 
@@ -37,17 +34,15 @@ contracts).
   - structs and enums
   - members
 - How to use the command, and what you'll get when you do.
-  - everything needed for that particular module in the format you see in
-    package documentation online
+  - everything needed for that particular module in the format you see in package documentation online
   - Only publishes public items
     - More on public/private next week
 
 ### `cargo doc`
 
-- auto-builds full project docs and drops them in `target/doc`, just like
-  `rustdoc`
-- the difference: automatically includes all public elements in a given
-  project, so you don't have to run `rustdoc` manually over and over again!
+- auto-builds full project docs and drops them in `target/doc`, just like `rustdoc`
+- the difference: automatically includes all public elements in a given project, so you don't have to run `rustdoc`
+  manually over and over again!
 - limitations for full sites
   - `rustbook` tool
   - Rust and Cargo sites built using other tools to pull the pieces together
--- src/e001.rs
+++ src/e001.rs
@@ -7,28 +7,25 @@
 //!
 //! [mp3]: https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e001.mp3
 //!
-//! <audio style="width: 100%" title="Document all the things!" controls preload=metadata src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e001.mp3"></audio>
+//! <audio style="width: 100%" title="Document all the things!" controls preload=metadata
+//! src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e001.mp3"></audio>
 //!
 //! # Notes
 //!
-//! This is a mostly-empty module, and it is intended as such. Why? Well,
-//! because *almost* all the sample code exists in these comments, which serve
-//! as the show notes. If you listen to the episode or take a look at the
+//! This is a mostly-empty module, and it is intended as such. Why? Well, because *almost* all the sample code exists
+//! in these comments, which serve as the show notes. If you listen to the episode or take a look at the
 //! [source files][src], you'll see how it works!
 //!
 //! [src]: ../../src/show_notes/e001.rs.html
 //!
-//! The components below are included *solely* so you can see how the
-//! docstrings work with each kind of thing. Make sure to click on the names of
-//! the items: there is more documentation there. Again, take a look at the
-//! [source][src] to see how it looks in the context of a file module.
+//! The components below are included *solely* so you can see how the docstrings work with each kind of thing. Make
+//! sure to click on the names of the items: there is more documentation there. Again, take a look at the [source][src]
+//! to see how it looks in the context of a file module.
 //!
-//! Note that this module-level docstring uses `//!` rather than `///`-style
-//! comments. This is because this docstring is documenting the item which
-//! contains it, rather than the following item. Per [Rust RFC 505][1], the
-//! preferred approach is always to use the "following" form (`///`) rather than
-//! the "containing" form (`//!`), except for module-level docs like these.
-//! (I will be following RFC 505 throughout.)
+//! Note that this module-level docstring uses `//!` rather than `///`-style comments. This is because this docstring
+//! is documenting the item which contains it, rather than the following item. Per [Rust RFC 505][1], the preferred
+//! approach is always to use the "following" form (`///`) rather than the "containing" form (`//!`), except for
+//! module-level docs like these. (I will be following RFC 505 throughout.)
 //!
 //! [1]: https://github.com/rust-lang/rfcs/blob/master/text/0505-api-comment-conventions.md
 //!
@@ -47,8 +44,8 @@
 //!   - Rust's package hosting: [crates.io][12]
 //!       + [Crater][13] for testing for backwards compatibility
 //!   - [Semantic versioning][14]
-//!       + ["Stability as a Deliverable"][15]: Rust official blog post on
-//!         version stability, backwards compatibility, and release channels.
+//!       + ["Stability as a Deliverable"][15]: Rust official blog post on version stability, backwards compatibility,
+//!         and release channels.
 //!   - [The Rust book chapter on `rustdoc`][16]
 //!
 //! [2]: https://github.com/rust-lang/rfcs/issues/1061
@@ -80,11 +77,11 @@
 
 /// This is a sample structure, to demonstrate `rustdoc`/`cargo doc`.
 ///
-/// All of this will be attached to the structure definition when you see it
-/// live on the site. If you need to document members, you can do it inline!
+/// All of this will be attached to the structure definition when you see it live on the site. If you need to document
+/// members, you can do it inline!
 ///
-/// It's worth taking a look at the [source] for this: you can see how private
-/// and public fields are handled differently in the documentation.
+/// It's worth taking a look at the [source] for this: you can see how private and public fields are handled
+/// differently in the documentation.
 ///
 /// [source]: ../../src/show_notes/e001.rs.html#89-96
 pub struct TVShow {
@@ -96,11 +93,9 @@ pub struct TVShow {
     director: String,
 }
 
-/// You can also document the implementation. This is usually *not* going to be
-/// something you particularly need with a `struct`, because you can just
-/// attach the documentation to the `struct` itself. However, it *is* the kind
-/// of thing that could be useful if you are implementing a `trait`. (More on
-/// that in a later episode!)
+/// You can also document the implementation. This is usually *not* going to be something you particularly need with a
+/// `struct`, because you can just attach the documentation to the `struct` itself. However, it *is* the kind of thing
+/// that could be useful if you are implementing a `trait`. (More on that in a later episode!)
 impl TVShow {
     /// This documents a fairly ho-hum structure constructor.
     ///
@@ -119,9 +114,8 @@ impl TVShow {
 
 /// This documents a plain-old function.
 ///
-/// The same basic rules apply as with functions bound to structs; the only
-/// real difference is that the docs for this won't be attached to a `struct`
-/// but to the module.
+/// The same basic rules apply as with functions bound to structs; the only real difference is that the docs for this
+/// won't be attached to a `struct` but to the module.
 pub fn use_members() {
     let firefly = TVShow::new("You can't take the sky from me!", 2001, "Joss Whedon");
     println!(

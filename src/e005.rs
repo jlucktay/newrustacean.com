7f846399ce8e2377dd5d63231cb70f43117961cb
James Lucktaylor <jlucktay+github@gmail.com>
2022-08-31 16:31:24 +0100

    style(e005): wrap at 120

--- src/e005.rs
+++ src/e005.rs
@@ -7,35 +7,30 @@
 //!
 //! [mp3]: https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e005.mp3
 //!
-//! <audio style="width: 100%" title="Allocate it where?" controls preload=metadata src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e005.mp3"></audio>
+//! <audio style="width: 100%" title="Allocate it where?" controls preload=metadata
+//! src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/e005.mp3"></audio>
 //!
 //! # Notes
 //!
-//! This episode, we look at returning functions from other functions, and as
-//! part of that discuss some basics about the stack and the heap---and why we
-//! need to care about them for returning functions.
+//! This episode, we look at returning functions from other functions, and as part of that discuss some basics about
+//! the stack and the heap---and why we need to care about them for returning functions.
 //!
-//! The functions themselves are not especially interesting; they just show you
-//! the basic form you use to return functions from other functions, and how to
-//! then use them in another function. You'll want to take a detailed look
-//! instead at the documentation for each (or just read the [source][notes-1]!),
-//! because that's where the meat of the discussion in this week's code is.
+//! The functions themselves are not especially interesting; they just show you the basic form you use to return
+//! functions from other functions, and how to then use them in another function. You'll want to take a detailed look
+//! instead at the documentation for each (or just read the [source][notes-1]!), because that's where the meat of the
+//! discussion in this week's code is.
 //!
 //! ## Corrigenda
 //!
-//!   - Steve Klabnik [pointed out][notes-2] that my description of `Vector`
-//!     types as totally heap-allocated was misleading. It's better to say that
-//!     the *contents* of the `Vector` -- its data -- is heap-allocated, while
-//!     the memory for the smart pointer and associated metadata are allocated
-//!     on the stack. I had this in mind, and sort of alluded to it earlier in
-//!     the discussion, but the way I actually said it was misleading at best.
+//!   - Steve Klabnik [pointed out][notes-2] that my description of `Vector` types as totally heap-allocated was
+//!     misleading. It's better to say that the *contents* of the `Vector` -- its data -- is heap-allocated, while the
+//!     memory for the smart pointer and associated metadata are allocated on the stack. I had this in mind, and sort
+//!     of alluded to it earlier in the discussion, but the way I actually said it was misleading at best.
 //!
-//!   - Chad Sharp (@crossroads1112 on GitHub) [clarified][notes-3] that in C99,
-//!     C *does* support dynamic array allocation, though it became optional for
-//!     compilers to implement it as of C11. I forgot about this because I spend
-//!     so much of my time dealing with Visual C++, which does not support
-//!     dynamic array allocation. (Notably, Visual C does; it is Visual C++
-//!     which does not: remember, C and C++ are related but distinct languages.)
+//!   - Chad Sharp (@crossroads1112 on GitHub) [clarified][notes-3] that in C99, C *does* support dynamic array
+//!     allocation, though it became optional for compilers to implement it as of C11. I forgot about this because I
+//!     spend so much of my time dealing with Visual C++, which does not support dynamic array allocation. (Notably,
+//!     Visual C does; it is Visual C++ which does not: remember, C and C++ are related but distinct languages.)
 //!
 //! Thanks to Steve and Chad for their helpful feedback!
 //!
@@ -83,11 +78,10 @@
 
 /// Creates a function which doubles an integer.
 ///
-/// Note that it doesn't have `Fn(i32) -> i32` as its return type, but rather
-/// `Box<Fn(i32) -> i32`. At present, functions have to be explicitly
-/// heap-allocated. If we tried to return just a `Fn` type, we'd end up in a
-/// tangled mess of lifetime and ownership (whether with a regular function, as
-/// here, or a closure as in `doubler_closure_factory`).
+/// Note that it doesn't have `Fn(i32) -> i32` as its return type, but rather `Box<Fn(i32) -> i32`. At present,
+/// functions have to be explicitly heap-allocated. If we tried to return just a `Fn` type, we'd end up in a tangled
+/// mess of lifetime and ownership (whether with a regular function, as here, or a closure as in
+/// `doubler_closure_factory`).
 pub fn doubler_factory() -> Box<dyn Fn(i32) -> i32> {
     /// The doubler function we will return.
     const fn double(n: i32) -> i32 {
@@ -98,11 +92,9 @@ pub fn doubler_factory() -> Box<dyn Fn(i32) -> i32> {
 
 /// Creates a closure which doubles an integer.
 ///
-/// Compare with the function above: this is basically identical to
-/// `doubler_factory`, the only difference being that in this case, we return a
-/// closure rather than a standalone function. That has substantial
-/// ramifications for using closures and named functions, of course; refer to
-/// the discussion in [e004][e004] for details
+/// Compare with the function above: this is basically identical to `doubler_factory`, the only difference being that
+/// in this case, we return a closure rather than a standalone function. That has substantial ramifications for using
+/// closures and named functions, of course; refer to the discussion in [e004][e004] for details.
 ///
 /// [e004]: /show_notes/e004/
 #[must_use]
@@ -123,10 +115,9 @@ pub fn doubler_closure_factory() -> Box<dyn Fn(i32) -> i32> {
 /// }
 /// ```
 ///
-/// And another. This gets us a bit closer, because you actually have both a
-/// concrete reference type and a lifetime. However, it still won't compile,
-/// because after `get_normal_function_with_lifetime` ends, the function goes
-/// out of scope and the reference points to junk value.
+/// And another. This gets us a bit closer, because you actually have both a concrete reference type and a lifetime.
+/// However, it still won't compile, because after `get_normal_function_with_lifetime` ends, the function goes out of
+/// scope and the reference points to junk value.
 ///
 /// ```rust,ignore
 /// fn get_normal_function_with_lifetime() -> &'static (Fn(i32) -> i32) {
@@ -138,9 +129,8 @@ pub fn doubler_closure_factory() -> Box<dyn Fn(i32) -> i32> {
 /// }
 /// ```
 ///
-/// Even this approach doesn't work, because although the *function* lives on
-/// past the end of the `get_normal_external_fn_with_lifetime`, the reference
-/// created during that function call *doesn't*.
+/// Even this approach doesn't work, because although the *function* lives on past the end of the
+/// `get_normal_external_fn_with_lifetime`, the reference created during that function call *doesn't*.
 ///
 /// ```rust,ignore
 /// fn multiply_by_4(n: i32) -> i32 { n * 4 }
@@ -150,8 +140,7 @@ pub fn doubler_closure_factory() -> Box<dyn Fn(i32) -> i32> {
 /// }
 /// ```
 ///
-/// The solution, as we saw at the beginning, is to use `Box::new` to
-/// heap-allocate the function result instead.
+/// The solution, as we saw at the beginning, is to use `Box::new` to heap-allocate the function result instead.
 pub fn demonstrate_function_returns() {
     let double = doubler_factory();
     println!("{:}", double(14));

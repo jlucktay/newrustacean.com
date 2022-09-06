4cd3d5156e79152ca3d723c89002fbaf3a2a7400
James Lucktaylor <jlucktay+github@gmail.com>
2022-09-06 12:44:35 +0100

    style(bonus-3): wrap at 120

--- src/bonus/_3.rs
+++ src/bonus/_3.rs
@@ -1,4 +1,4 @@
-//! Building as a community
+//! # Building as a community
 //!
 //!   - **Date:** January 31, 2016
 //!   - **Subject:** The value of a good community, and how you can help Rust today.
@@ -6,19 +6,17 @@
 //!
 //! [mp3]: https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/bonus/3.mp3
 //!
-//! <audio style="width: 100%" title="Building as a community" controls preload=metadata src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/bonus/3.mp3"></audio>
+//! <audio style="width: 100%" title="Building as a community" controls preload=metadata
+//! src="https://www.podtrac.com/pts/redirect.mp3/cdn.newrustacean.com/file/newrustacean/bonus/3.mp3"></audio>
 //!
-//! Notes
-//! -----
+//! ## Notes
 //!
-//! Community is one of the most important parts of a programming language
-//! community, or indeed *any* technical community. In this episode, I talk
-//! a bit about what happens when you don't have a good community, how Rust's
-//! community has done well so far, and then how to keep building a good
-//! community and how to build good things *as* a community.
+//! Community is one of the most important parts of a programming language community, or indeed *any* technical
+//! community. In this episode, I talk a bit about what happens when you don't have a good community, how Rust's
+//! community has done well so far, and then how to keep building a good community and how to build good things *as* a
+//! community.
 //!
-//! Sponsors
-//! --------
+//! ## Sponsors
 //!
 //!   - Aleksey Pirogov
 //!   - Chris Palmer
@@ -40,8 +38,7 @@
 //!   - [Dwolla](https://www.dwolla.com/hub/chriskrycho)
 //!   - [Cash.me](https://cash.me/$chriskrycho)
 //!
-//! Follow
-//! ------
+//! ## Follow
 //!
 //!   - New Rustacean:
 //!     + Twitter: [@newrustacean](https://www.twitter.com/newrustacean)
